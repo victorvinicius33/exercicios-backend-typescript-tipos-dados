3e1b9617effb45f7756354ef41720ee51c57076f
Victor Dell <victorvinicius33.vv@gmail.com>
2024-07-25 09:30:09 -0300

    01 done

--- 01/scripts.ts
+++ 01/scripts.ts
@@ -19,22 +19,22 @@ const usuarios = [
         nome: "Antonio",
         pets: ["Naninha"],
     },
-];
-
-const buscarDonoPet = (lista, nomePet) => {
+  ];
+  
+  const buscarDonoPet = (lista: { nome: string, pets: string[]}[], nomePet: string) => {
     let usuarioEncontrado;
-
+  
     for (const usuario of lista) {
         if (usuario.pets.includes(nomePet)) {
             usuarioEncontrado = usuario;
         }
     }
-
+  
     if (usuarioEncontrado) {
         console.log(`O dono(a) do(a) ${nomePet} é o(a) ${usuarioEncontrado.nome}`);
     } else {
         console.log(`Que pena ${nomePet}, não encontramos seu dono(a)`);
     }
-}
-
-buscarDonoPet(usuarios, 'Guido');
+  }
+  
+  buscarDonoPet(usuarios, 'Guido');
