676eff9545c307aec4a96b51183a38b3adcb1166
Victor Dell <victorvinicius33.vv@gmail.com>
2024-07-25 09:30:52 -0300

    02 done

---
+++ 02/scripts.ts
@@ -0,0 +1,56 @@
+const users = [
+  {
+      nome: "Guido",
+      idade: 32,
+      status: true,
+  },
+  {
+      nome: "Dani",
+      idade: 30,
+      status: true,
+  },
+  {
+      nome: "João",
+      idade: 40,
+      status: false,
+  },
+  {
+      nome: "Guilherme",
+      idade: 29,
+      status: true,
+  },
+  {
+      nome: "Ana",
+      idade: 18,
+      status: false,
+  },
+  {
+      nome: "José",
+      idade: 28,
+      status: false,
+  },
+]
+
+const filterUsers = (usersList: { nome: string, idade: number, status: boolean }[], nameFilter: string) => {
+  let filteredUsers: { nome: string, idade: number, status: boolean }[] = [];
+
+  usersList.forEach(user => {
+    let usernameMatchesFilter: boolean = true;
+    
+    for(let i = 0; i < nameFilter.length - 1; i++) {
+      if (user.nome[i].toLowerCase() !== nameFilter[i].toLowerCase()) {
+        usernameMatchesFilter = false;
+        break;
+      }
+    }
+
+    if (usernameMatchesFilter === true) {
+      filteredUsers.push(user);
+    }
+  });
+
+
+  return filteredUsers;
+}
+
+console.log(filterUsers(users, 'Jo'));
